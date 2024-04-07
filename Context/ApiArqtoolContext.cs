329c2e3385042f9e8cf914c421013dc7d8c53a62
Caioliu <caioarthur_@hotmail.com>
2024-04-07 12:04:20 -0300

    definindo classe context e conexão com banco de dados

--- Context/ApiArqtoolContext.cs
+++ Context/ApiArqtoolContext.cs
@@ -1,4 +1,8 @@
-﻿namespace caiobadev_api_arqtool.Context {
-    public class ApiArqtoolContext {
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace caiobadev_api_arqtool.Context {
+    public class ApiArqtoolContext : DbContext {
+        public ApiArqtoolContext(DbContextOptions<ApiArqtoolContext> options) : base(options) { }
+        
     }
 }
